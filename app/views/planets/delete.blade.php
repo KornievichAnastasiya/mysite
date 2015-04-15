2be0a0969c20bdfe9725eb9d519723cccf6a3583
KornievichAnastasiya <anastasiya.ups@gmail.com>
2015-04-15 18:02:10 +0200

    rest

--- app/views/planets/delete.blade.php
+++ app/views/planets/delete.blade.php
@@ -1,17 +1,17 @@
 @extends('layout')
 
 @section('title')
-   Планете удалена
+ @lang('planet_title_delete')
 @stop
 
 @section('content')
     <div class="jumbotron">
         <div class="container">
-            <h1>Congratulations, planets "{{$planetName}}" deleted!</h1>
+            <h1>@lang('planet.deleted_planet') "{{$planetName}}" deleted!</h1>
 
             <div>
-                <a href="{{{URL::action('PlanetsController@store')}}}">← Create new planets</a>/
-                <a href="{{Config::get('app.url')}}">Go Home → </a>
+                <a href="{{{URL::action('PlanetsController@create')}}}">← Create new planets</a>/
+                <a href="{{Config::get('app.url')}}">@lang('action.home') → </a>
             </div>
         </div>
     </div>
