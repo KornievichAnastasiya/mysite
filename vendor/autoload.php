5979bf3564ff7d0c3257b203636aa2f6bbb146de
KornievichAnastasiya <anastasiya.ups@gmail.com>
2015-05-05 18:53:27 +0200

    pagination

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit6bc8c141cb3d5597520d6803871e1943::getLoader();
+return ComposerAutoloaderInit464a1463914f06f3e8c13c2f1bcdbc83::getLoader();
